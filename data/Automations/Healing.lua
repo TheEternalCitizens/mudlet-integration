79d384ae8bbac43d186f8322c34929d72b5878c3
davewiththenicehat <54369722+davewiththenicehat@users.noreply.github.com>
2022-11-25 16:53:18 -0500

    automation default scripts

--- data/Automations/Healing.lua
+++
@@ -1,5 +0,0 @@
-return {{[1] = {2},[2] = {3},[3] = {4},},
-{[ [[automationTrigger]] ] = [[0]],[ [[automationCommand]] ] = [[diagpart]],[ [[repeatOnFail]] ] = 1,},
-{[ [[automationTrigger]] ] = [[0]],[ [[automationCommand]] ] = [[bandpart]],[ [[repeatOnFail]] ] = 1,},
-{[ [[automationTrigger]] ] = [[0]],[ [[automationCommand]] ] = [[rmbanpart]],[ [[repeatOnFail]] ] = 1,},
-}
--- data/Interrupts/WaitForMonlon.lua
+++
@@ -1,3 +0,0 @@
-return {{[1] = {2},[ [[enabled]] ] = false,[ [[interruptRepeat]] ] = [[1]],[ [[pattern]] ] = [[^A sailor calls out, &quot;All hands prepare to depart!  Next destination, the city of Monlon!$]],[ [[firstCommandTrigger]] ] = [[1]],},
-{[ [[automationTrigger]] ] = [[1]],[ [[automationCommand]] ] = [[wait for dock]],[ [[repeatOnFail]] ] = 0,},
-}
